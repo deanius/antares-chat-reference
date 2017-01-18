d54cef26788bfa19f8d35c3f5ed3345899a2467b
Dean Radcliffe <deanmisc@yahoo.com>
2017-01-18 16:26:18 -0600

    renaming

--- base/actions.js
+++ base/actions.js
@@ -1,5 +1,5 @@
 export default {
-    Conversation: {
+    Chat: {
         start: () => ({
             type: 'Antares.store',
             payload: { senders: ['Self', 'Other'] }
--- base/antares.js
+++ base/antares.js
@@ -9,6 +9,6 @@ export const announce = Antares.announce
 inAgencyRun('any', function() {
     Object.assign(this, {
         Antares,
-        Actions: AntaresConfig
+        Actions: AntaresConfig.Actions
     })
 })
