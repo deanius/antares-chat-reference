525fd4e672d34bb37cc01423c152d0a686af0377
Dean Radcliffe <deanmisc@yahoo.com>
2017-01-18 23:37:45 -0600

    feat: Impl and Subscribing of a MongoRender

--- base/antares.js
+++ base/antares.js
@@ -1,14 +1,45 @@
 import { AntaresMeteorInit, AntaresInit, inAgencyRun } from 'meteor/deanius:antares'
+import { Mongo } from 'meteor/mongo'
 import * as AntaresConfig from './antares-config'
 
 export const Antares = AntaresMeteorInit(AntaresInit)(AntaresConfig)
 export const store = Antares.store
 export const announce = Antares.announce
 
+const Collections = {
+    Chats: new Mongo.Collection('chats')
+}
+
 // Extend our global scope with Antares in client and server agencies
 inAgencyRun('any', function() {
     Object.assign(this, {
         Antares,
-        Actions: AntaresConfig.Actions
+        Actions: AntaresConfig.Actions,
+        Collections
+    })
+})
+
+const MongoRenderer = ({ mongoDiff }) => {
+    let { id, collection, update, upsert, updateOp } = mongoDiff
+
+    let MongoColl = Collections[collection]
+    if (!MongoColl) throw new Error(`Collection ${collection} not found`)
+
+    if (update) {
+        MongoColl.update(
+            { _id: id },
+            updateOp,
+            {
+                upsert
+            }
+        )
+    }
+}
+
+inAgencyRun('server', () => {
+    Antares.subscribeRenderer(MongoRenderer, {
+        mode: 'sync',
+        xform: diff$ => diff$
+            .filter(({ mongoDiff }) => mongoDiff !== null)
     })
 })
